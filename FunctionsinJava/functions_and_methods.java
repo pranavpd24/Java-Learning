e5b2ffbafaf3870f5254138dccf6fc7c43e6dfd5
pranavpd24 <digraskarpranav@gmail.com>
2025-05-12 00:46:06 +0530

    Add functions_and_methods.java

---
+++ FunctionsinJava/functions_and_methods.java
@@ -0,0 +1,13 @@
+public class functions_and_methods {
+
+    public static void main(String[] args) {
+        int value = sum(4,4);
+        System.out.println(value);
+    }
+    static int sum(int a , int b){
+        int sum = a+b;
+        return sum;
+
+
+    }
+}
